c83398850541c47365681fec22ffc13db9bb589f
Vamshikrishna0372 <nagulavamshi1453@gmail.com>
2026-04-03 01:00:04 +0530

    updating complete code

--- StartupIQ_frontend/src/services/api.ts
+++ StartupIQ_frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8002',
+  baseURL: 'https://startupiq-nkxn.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
